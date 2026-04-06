cf78803d7a61e236bd01e580f45c0dd7cd5c8245
Andrew <bida.andres14@gmail.com>
2026-04-06 23:06:16 +0300

    Add subscription models and admin registration

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-# додані апки 
+
     'rest_framework',
     'branches',
     'lessons',
--- Project/subscriptions/models.py
+++ Project/subscriptions/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+from branches.models import Branch, Subject
+from students.models import Student
+class SubscriptionPlan(models.Model):
+    name = models.CharField(max_length=255)
+    PLAN_CHOICES = [
+        ('group', 'Групові заняття'),
+        ('individual', 'Індивідуальні заняття'),
+    ]
+    plan_type = models.CharField(max_length=255, choices=PLAN_CHOICES)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='plans')
+    def __str__(self):
+        return f"{self.name} — {self.branch.name} ({self.get_plan_type_display()})"
 
-# Create your models here.
+class StudentSubscription(models.Model):
+    start_date = models.DateField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='subscriptions')
+    subscription = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE, related_name='student_subscriptions')
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='subject_subscriptions')
+    def __str__(self):
+        return f"{self.student} — {self.subscription.name} ({self.subject.name})"
+    
